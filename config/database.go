edb5ca0bbeddb50565a60f0e23f6ab201d6dbfc4
abdulhalim29k <abdulhalim29k@gmail.com>
2025-12-30 00:40:33 +0700

    Update for PostgreSQL and Render deployment

--- config/database.go
+++ config/database.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"github.com/joho/godotenv"
 )
 
@@ -14,14 +14,28 @@ func LoadEnv() {
 }
 
 func ConnectDB() (*sql.DB, error) {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL != "" {
+		// Production: use DATABASE_URL
+		db, err := sql.Open("postgres", dbURL)
+		if err != nil {
+			return nil, err
+		}
+		if err = db.Ping(); err != nil {
+			return nil, err
+		}
+		return db, nil
+	}
+
+	// Development: use individual env vars
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
-	db, err := sql.Open("mysql", dsn)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require", dbHost, dbPort, dbUser, dbPassword, dbName)
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
